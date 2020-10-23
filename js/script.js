3b45bde1d7b32bf1b388c95ca8f777375a2967c8
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-22 22:44:54 -0300

    creo las funciones fundamentales
    estas funciones van a trabajar los datos desde el Local Storage e interactuarán en la interfaz (a traves del DOM).

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ console.log("Hay que linkearlo al index.html")
 function sumar(num1,num2){
     return num1 + num2
 }
+
+console.log("Hay que linkearlo al index.html")
+
+function sumar(num1,num2){
+    return num1 + num2
+}
+
+
+function guardarDatos(){
+
+}
+
+function mostrarDatos(){
+
+}
+
+function eliminarItems(params) {
+    localStorage.removeItem('elItemEnCuestion');
+}
+
+function eliminarTodo(params) {
+    localStorage.clear();
+}
