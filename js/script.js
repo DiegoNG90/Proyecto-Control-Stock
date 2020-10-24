6d4a08d39fba98629351a446eb46f5cc32469aae
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-23 21:22:40 -0300

    validar usuario
    Eliminé la funcion sumar(era de prueba, no la necesitaba) y cree un evento asociado al click del boton log in que chequee que el usuario y la contraseña son "validas" (es decir, que existan en el local storage dentro de un objeto/array usuarios).

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-console.log("Hay que linkearlo al index.html")
+console.log("Linkeado al index.html")
+console.log("Linkeado al login.html")
 
-function sumar(num1,num2){
-    return num1 + num2
+const superUsuario = {
+    user: "diego@hotmail.com",
+    password: 123456
 }
 
-console.log("Hay que linkearlo al index.html")
+localStorage.setItem('superUsuario', JSON.stringify(superUsuario));
+let userRegistrado = localStorage.getItem('superUsuario');
+userRegistrado = JSON.parse(userRegistrado);
+// console.log(userRegistrado);
 
-function sumar(num1,num2){
-    return num1 + num2
+const $nodoBotonLogIn = $('#log-in')[0];
+
+$nodoBotonLogIn.onclick = function (e){
+    e.preventDefault();
+    
+    const $nodoMail = $('#inputEmail')[0].value;
+    const $nodoPass = $('#inputPassword')[0].value;
+
+    if ($nodoMail === userRegistrado.user && $nodoPass === userRegistrado.password.toString()) {
+        window.location.href = "index.html";
+    }
 }
+
+
 
 
 function guardarDatos(){
