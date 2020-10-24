326bc2b253e061d068d79c002952b979ecc1d3ec
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-23 21:21:01 -0300

    logica de creacion de usuario
    Work in progress:
    Crear una funcion atada a un evento on click que cree un usuario nuevo (y lo agregue a un objeto/array de usuarios registrados)  si y sólo si el usuario no existe (existe el user Diego nomás).

---
+++ js/registro.js
@@ -0,0 +1,10 @@
+// const usuarioNuevo = $('#crearEmail')[0].value;
+// const contraseñaUNuevo$ = ('#crearPassword')[0].value;
+// const confirmaContraseñaUNuevo = $('#confirmarPassword')[0].value;
+
+const $botonRegistro = $('#btn-registrarse')[0];
+$botonRegistro.onclick = function(e){
+    e.preventDefault();
+
+
+}
