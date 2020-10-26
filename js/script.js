9a3816b0a04cba724b1e18ed024248e769183ebd
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-26 20:09:59 -0300

    Super usuario, evento login y WIP
    - Creo un superUsuario para chequear que ande el loggin
    - Creo un evento basado en if/else control flow: para chequear si el superUsuario puede loggearse exitosamenete o si no puede. Si no puede, mostrar errores, pintar cajas de los inputs y texto de erro de rojo.
    
    -WORK IN PROGRESS:
    Siguiente paso; crear una array (ALLUSERS) que vaya adjuntando al superUsuario y a la creacion de otros usuarios (registro.js).

--- js/script.js
+++ js/script.js
@@ -6,10 +6,14 @@ const superUsuario = {
     password: 123456
 }
 
+// const ALLUSERS = [];
+// ALLUSERS.push(superUsuario);
+
 localStorage.setItem('superUsuario', JSON.stringify(superUsuario));
 let userRegistrado = localStorage.getItem('superUsuario');
 userRegistrado = JSON.parse(userRegistrado);
 // console.log(userRegistrado);
+
 
 const $nodoBotonLogIn = $('#log-in')[0];
 
@@ -18,10 +22,38 @@ $nodoBotonLogIn.onclick = function (e){
     
     const $nodoMail = $('#inputEmail')[0].value;
     const $nodoPass = $('#inputPassword')[0].value;
-
+    const $errores = $('#errores')[0];
+    //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
+    while($errores.firstChild){
+        $errores.removeChild($errores.firstChild);
+    };
+    
+    //Si coinciden:
     if ($nodoMail === userRegistrado.user && $nodoPass === userRegistrado.password.toString()) {
         window.location.href = "index.html";
     }
+    else {
+        let error = document.createElement("li");
+        error.classList.add('error-texto')
+        
+        
+        $errores.appendChild(error);
+        //Si el pass coincide pero el mail no: 
+        if($nodoMail !== userRegistrado.user && $nodoPass === userRegistrado.password.toString() ){
+            error.innerText = "El mail/usuario es incorrecto."
+            $('#inputEmail')[0].className = 'error-input';
+            $('#inputPassword')[0].className = 'form-control';
+        }//Si el mail coincide pero el pass no:
+        else if ($nodoMail === userRegistrado.user && $nodoPass !== userRegistrado.password.toString()) {
+            error.innerText = "El password es incorrecto."
+            $('#inputPassword')[0].className = 'error-input';
+            $('#inputEmail')[0].className = 'form-control';
+        }else{
+            error.innerText = "El mail y la contraseña son incorrectos."
+            $('#inputPassword')[0].className = 'error-input';
+            $('#inputEmail')[0].className = 'error-input';
+        }
+    }  
 }
 
 
