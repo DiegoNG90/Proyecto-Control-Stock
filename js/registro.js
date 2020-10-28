e2013500ebbcfd97d2dade847da1f20d95e60b66
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-28 16:37:12 -0300

    refactorizacion y funciones
    -Se crea funcion chequearMailExistente para ver si ese usuario ya existe en el LS
    -chequearYBorrarErrores para eliminar estilos y contenido de los div asociados a la existencia de errores.
    
    WIK:
    Pensar la manera de guardar newUser dentro de una array de usuarios (tipo ALLUSERS o algo asi);

--- js/registro.js
+++ js/registro.js
@@ -3,26 +3,90 @@ const $botonRegistro = $('#btn-registrarse')[0];
 
 $botonRegistro.onclick = function(e){
     e.preventDefault();
-    //Traer la array guardada en localStorage
-    localStorage.getItem('ALLUSERS');
-    let usersRegistrados = localStorage.getItem('ALLUSERS');
-    usersRegistrados = JSON.parse(usersRegistrados);
     
+
     //crear los nodos del form
     const usuarioNuevo = $('#crearEmail')[0].value;
     const contraseñaUNuevo = $('#crearPassword')[0].value;
     const confirmaContraseñaUNuevo = $('#confirmarPassword')[0].value;
     const erroresRegistro = $('#registro-errores')[0];
+    //borrar errores previos
+    chequearYBorrarErrores(erroresRegistro);
+    //Iterar la array
+    chequearMailExistente(chequearYBorrarErrores, erroresRegistro, usuarioNuevo);
+    //2) SI NO EXISTE, crearlo y borrar posibles errores y clases 'error-texto' según el caso.
+    if ((chequearMailExistente(chequearYBorrarErrores, erroresRegistro,usuarioNuevo) !== usuarioNuevo)) {
+        //alert(`Bien, usted ha creado el usuario ${usuarioNuevo}`)
 
+        //borro posibles errores previos
+        $('#crearEmail')[0].classList.remove('error-input');
+        $('#crearEmail')[0].className = 'form-control';
+
+        //Si el mail es nuevo pero las contraseñas difieren
+        if(contraseñaUNuevo !== confirmaContraseñaUNuevo){
+            chequearYBorrarErrores(erroresRegistro);
+
+            let errorRegistro = document.createElement("li");
+            errorRegistro.classList.add('error-texto');
+            errorRegistro.innerText = "Las contraseñas no coinciden. Escribalas nuevamente."
+            $('#crearPassword')[0].className = 'error-input';
+            $('#confirmarPassword')[0].className = 'error-input';
+
+            erroresRegistro.appendChild(errorRegistro);
+
+        }//
+        else {
+            //eliminar rastros de estilo de errores
+            chequearYBorrarErrores(erroresRegistro);
+            $('#crearPassword')[0].className = 'form-control';
+            $('#confirmarPassword')[0].className = 'form-control';
+
+            const newUser = {
+                user: `${usuarioNuevo}`,
+                password: Number(`${confirmaContraseñaUNuevo}`)
+            }
+            
+            //Subo al newUser al LS
+            localStorage.setItem('newUser', JSON.stringify(newUser));
+            
+            $('#main-registro')[0].className = "oculto";
+            $('#esExito')[0].classList.remove('oculto');
+            $('body')[0].style.backgroundColor = "#311c1c";
+
+            redireccionar();
+        }
+    }
+}
+
+function redireccionar(){
+    setTimeout(function(){window.location.href = "login.html";}, 2500);
+}
+
+function exportarUsersRegistrados(){
+
+}
+
+function chequearYBorrarErrores(input){
     //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
-    while(erroresRegistro.firstChild){
-        erroresRegistro.removeChild(erroresRegistro.firstChild);
+    while(input.firstChild){
+        input.removeChild(input.firstChild);
     };
+}
 
-    //Iterar la array usersRegistrados
-    for (const element of usersRegistrados) {
-        //1) Chequear si el mail/usuario existe
-        if(element.user === usuarioNuevo){
+
+function chequearMailExistente(chequearYBorrarErrores, erroresRegistro,usuarioNuevo){
+    const ALLUSERS = [];
+    const mails = [];
+    const pass = [];
+
+    for(let i=0; i < Object.values(localStorage).length; i++){
+        ALLUSERS.push(JSON.parse(Object.values(localStorage)[i]));
+        mails.push(ALLUSERS[i].user)
+        pass.push(ALLUSERS[i].password)
+
+        if(mails[i] === usuarioNuevo){
+            chequearYBorrarErrores(erroresRegistro);
+
             //alert("El usuario ya existe, ingrese otro mail/user.")
             let errorRegistro = document.createElement("li");
             errorRegistro.classList.add('error-texto');
@@ -32,61 +96,9 @@ $botonRegistro.onclick = function(e){
             erroresRegistro.appendChild(errorRegistro);
             $('#crearPassword')[0].className = 'form-control';
             $('#confirmarPassword')[0].className = 'form-control';
-
-
-        }//2) SI NO EXISTE, crearlo y borrar posibles errores y clases 'error-texto' según el caso.
-        else {
-            //alert(`Bien, usted ha creado el usuario ${usuarioNuevo}`)
-
-            //borro posibles errores previos
-            $('#crearEmail')[0].classList.remove('error-input');
-            $('#crearEmail')[0].className = 'form-control';
-
-            //Si el mail es nuevo pero las contraseñas difieren
-            if(contraseñaUNuevo !== confirmaContraseñaUNuevo){
-                let errorRegistro = document.createElement("li");
-                errorRegistro.classList.add('error-texto');
-                errorRegistro.innerText = "Las contraseñas no coinciden. Escribalas nuevamente."
-                $('#crearPassword')[0].className = 'error-input';
-                $('#confirmarPassword')[0].className = 'error-input';
-
-                erroresRegistro.appendChild(errorRegistro);
-
-            }//
-            else {
-                //eliminar rastros de estilo de errores
-                $('#crearPassword')[0].className = 'form-control';
-                $('#confirmarPassword')[0].className = 'form-control';
-
-                const newUser = {
-                    user: `${usuarioNuevo}`,
-                    password: `${confirmaContraseñaUNuevo}`
-                }
-                
-                let usersRegistrados = localStorage.getItem('ALLUSERS');
-                usersRegistrados = JSON.parse(usersRegistrados);
-                usersRegistrados.push(newUser);
-
-                localStorage.setItem('ALLUSERS', JSON.stringify(usersRegistrados));
-                
-                $('#main-registro')[0].className = "oculto";
-                $('#esExito')[0].classList.remove('oculto');
-                $('body')[0].style.backgroundColor = "#311c1c";
-
-                break;
-            }
             
-
+            return mails[i];
+            break;
         }
-            
     }
-    redireccionar();
-}
-
-function redireccionar(){
-    setTimeout(function(){window.location.href = "login.html";}, 2500);
-}
-
-function exportarUsersRegistrados(){
-
 }
