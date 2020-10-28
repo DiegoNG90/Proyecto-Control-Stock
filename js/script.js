74a3ca69d583998f3d74ff83e1e239ec5244e042
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-28 16:39:03 -0300

    refactorizacion de las validaciones
    - Se incorpora un for loop para que itere las values de las keys del localStorage (que es un objeto. ie: window.localStorage).

--- js/script.js
+++ js/script.js
@@ -9,10 +9,19 @@ const superUsuario = {
 // const ALLUSERS = [];
 // ALLUSERS.push(superUsuario);
 
+//Subo el superusario al LS
 localStorage.setItem('superUsuario', JSON.stringify(superUsuario));
-let userRegistrado = localStorage.getItem('superUsuario');
-userRegistrado = JSON.parse(userRegistrado);
+
+//Guardo el LS en una variable para trabajarlo acá en .js
+// let userRegistrado = localStorage.getItem('superUsuario');
+//Lo parseo para poder trabajarlo
+// userRegistrado = JSON.parse(userRegistrado);
+//Subo userRegistrado al LS
+// localStorage.setItem('userRegistrado', JSON.stringify(userRegistrado));
 // console.log(userRegistrado);
+// localStorage.setItem('ALLUSERS', JSON.stringify(ALLUSERS));
+
+//Está subido superUsuario y userRegistrado
 
 
 const $nodoBotonLogIn = $('#log-in')[0];
@@ -23,37 +32,56 @@ $nodoBotonLogIn.onclick = function (e){
     const $nodoMail = $('#inputEmail')[0].value;
     const $nodoPass = $('#inputPassword')[0].value;
     const $errores = $('#errores')[0];
-    //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
-    while($errores.firstChild){
-        $errores.removeChild($errores.firstChild);
-    };
     
-    //Si coinciden:
-    if ($nodoMail === userRegistrado.user && $nodoPass === userRegistrado.password.toString()) {
-        window.location.href = "index.html";
-    }
-    else {
-        let error = document.createElement("li");
-        error.classList.add('error-texto')
-        
-        
-        $errores.appendChild(error);
-        //Si el pass coincide pero el mail no: 
-        if($nodoMail !== userRegistrado.user && $nodoPass === userRegistrado.password.toString() ){
-            error.innerText = "El mail/usuario es incorrecto."
-            $('#inputEmail')[0].className = 'error-input';
-            $('#inputPassword')[0].className = 'form-control';
-        }//Si el mail coincide pero el pass no:
-        else if ($nodoMail === userRegistrado.user && $nodoPass !== userRegistrado.password.toString()) {
-            error.innerText = "El password es incorrecto."
-            $('#inputPassword')[0].className = 'error-input';
-            $('#inputEmail')[0].className = 'form-control';
-        }else{
-            error.innerText = "El mail y la contraseña son incorrectos."
-            $('#inputPassword')[0].className = 'error-input';
-            $('#inputEmail')[0].className = 'error-input';
+    
+    const ALLUSERS = [];
+    const mails = [];
+    const pass = [];
+    //Iterar la array
+    for(let i=0; i < Object.values(localStorage).length; i++){
+        ALLUSERS.push(JSON.parse(Object.values(localStorage)[i]));
+        mails.push(ALLUSERS[i].user)
+        pass.push(ALLUSERS[i].password)
+
+        //Si coinciden:
+        if ($nodoMail === mails[i] && $nodoPass === pass[i].toString()) {
+            window.location.href = "index.html";
         }
-    }  
+        else {
+            //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
+            while($errores.firstChild){
+                $errores.removeChild($errores.firstChild);
+            };
+
+            let error = document.createElement("li");
+            error.classList.add('error-texto')
+            
+            
+            $errores.appendChild(error);
+            //Si el pass coincide pero el mail no: 
+            if($nodoMail !== mails[i] && $nodoPass === pass[i].toString() ){
+                error.innerText = "El mail/usuario es incorrecto."
+                $('#inputEmail')[0].className = 'error-input';
+                $('#inputPassword')[0].className = 'form-control';
+            }//Si el mail coincide pero el pass no:
+            else if ($nodoMail === mails[i] && $nodoPass !== pass[i].toString()) {
+                error.innerText = "El password es incorrecto."
+                $('#inputPassword')[0].className = 'error-input';
+                $('#inputEmail')[0].className = 'form-control';
+            } //Si los dos campos están vacíos:
+            else if (!$nodoMail && !$nodoPass) {
+                error.innerText = "El mail y la contraseña están incompletos."
+                $('#inputPassword')[0].className = 'error-input';
+                $('#inputEmail')[0].className = 'error-input';
+            }
+            else{
+                error.innerText = "El mail y la contraseña son incorrectos."
+                $('#inputPassword')[0].className = 'error-input';
+                $('#inputEmail')[0].className = 'error-input';
+            }
+        }  
+    }    
+    
 }
 
 
