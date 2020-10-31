d17d928da00b6faf3a513e2aa9b8ca3b53791ca9
DiegoNG90 <diego.nicolas.gandara@gmail.com>
2020-10-31 18:21:18 -0300

    Refactorizacion de la VALIDACION DEL LOGIN
    1) Elimino el objeto superUsuario (es innecesario, fue creado para hacer pruebas) y lo bajo del LS.
    
    2) Refactorizo las validaciones del mail en particular y toda la estructura del if/else que valida el mail y el password.
    
    3) Elimino la funcion chequearMailExistente que fue "importada" de registro.js dado que es útil en ese script, pero en éste ya fue resuelto desde la validación, dentro de la validación del evento onclick del boton $nodoBotonLogIn.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,14 @@
 console.log("Linkeado al index.html")
 console.log("Linkeado al login.html")
 
-const superUsuario = {
-    user: "diego@hotmail.com",
-    password: "123456"
-}
-
 //SI! AHORA SI!
 let ALLUSERS = [];
 ALLUSERS = JSON.parse(localStorage.getItem('ALLUSERS'));
 
-//Subo el superusario al LS
-localStorage.setItem('superUsuario', JSON.stringify(superUsuario));
-
-//Guardo el LS en una variable para trabajarlo acá en .js
-// let userRegistrado = localStorage.getItem('superUsuario');
-//Lo parseo para poder trabajarlo
-// userRegistrado = JSON.parse(userRegistrado);
-//Subo userRegistrado al LS
-// localStorage.setItem('userRegistrado', JSON.stringify(userRegistrado));
-// console.log(userRegistrado);
-// localStorage.setItem('ALLUSERS', JSON.stringify(ALLUSERS));
-
-//Está subido superUsuario y userRegistrado
-
-
 const $nodoBotonLogIn = $('#log-in')[0];
 
+
+//VALIDAR LOGIN
 $nodoBotonLogIn.onclick = function (e){
     e.preventDefault();
     
@@ -35,18 +17,27 @@ $nodoBotonLogIn.onclick = function (e){
     const $errores = $('#errores')[0];
     
     
-    const ALLUSERS = [];
+    const ALLUSERS = JSON.parse(localStorage.getItem('ALLUSERS'));
     const mails = [];
     const pass = [];
-    //Iterar la array
-    for(let i=0; i < Object.values(localStorage).length; i++){
-        ALLUSERS.push(JSON.parse(Object.values(localStorage)[i]));
+    for(let i=0; i < ALLUSERS.length; i++){
         mails.push(ALLUSERS[i].user)
         pass.push(ALLUSERS[i].password)
     
-        //Si coinciden:
+        //Si coinciden (LOGIN EXITOSO!):
         if ($nodoMail === mails[i] && $nodoPass === pass[i]) {
+            let logueado = localStorage.getItem('LOGIN');
+
+            if (logueado === null) {
+                logueado = true;
+                const LOGIN = localStorage.setItem('LOGIN', JSON.stringify(logueado))
+            }else{
+                const LOGIN = logueado;
+                localStorage.setItem('LOGIN', JSON.stringify(LOGIN));
+            }
+
             window.location.href = "index.html";
+
         }
         else {
             //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
@@ -56,60 +47,52 @@ $nodoBotonLogIn.onclick = function (e){
             error.classList.add('error-texto')
             
             $errores.appendChild(error);
-            //Si el pass coincide pero el mail no: 
-            if($nodoMail !== mails[i] && $nodoPass === pass[i]){
-                error.innerText = "El mail/usuario es incorrecto."
-                $('#inputEmail')[0].className = 'error-input';
-                $('#inputPassword')[0].className = 'form-control';
-            }//Si el mail coincide pero el pass no:
+ 
+            //Si alguno de los campos está vacío:
+            if (!$nodoMail || !$nodoPass) {
+                //Si ambos están vacíos
+                if (!$nodoMail && !$nodoPass) {
+                    error.innerText = "El mail/usuario y la contraseña están incompletos."
+                    $('#inputPassword')[0].className = 'error-input';
+                    $('#inputEmail')[0].className = 'error-input';
+                    break;
+                } else if (!$nodoMail && $nodoPass !== "") {
+                    error.innerText = "El mail/usuario está incompleto."
+                    $('#inputMail')[0].className = 'error-input';
+                    break;
+                }else{
+                    error.innerText = "La contraseña está incompleta."
+                    $('#inputPassword')[0].className = 'error-input';
+                    break;
+                }
+            }
+            //Si el mail coincide pero el pass no:
             else if ($nodoMail === mails[i] && $nodoPass !== pass[i]) {
                 error.innerText = "El password es incorrecto."
                 $('#inputPassword')[0].className = 'error-input';
                 $('#inputEmail')[0].className = 'form-control';
-            } //Si los dos campos están vacíos:
-            else if (!$nodoMail && !$nodoPass) {
-                error.innerText = "El mail y la contraseña están incompletos."
+                break;
+            }
+            //Si el pass coincide pero el mail no:
+            else if($nodoMail !== mails[i] && $nodoPass === pass[i]){
+                error.innerText = "El mail/usuario es incorrecto."
+                $('#inputEmail')[0].className = 'error-input';
+                $('#inputPassword')[0].className = 'form-control';
+                break;
+            } 
+            // Si el mail y el pass no coinciden
+            else if($nodoMail !== mails[i] && $nodoPass !== pass[i]){
+                error.innerText = "El mail/usuario y la contraseña no existen."
                 $('#inputPassword')[0].className = 'error-input';
                 $('#inputEmail')[0].className = 'error-input';
+                break;
             }
             else{
-                error.innerText = "El mail y la contraseña son incorrectos."
-                $('#inputPassword')[0].className = 'error-input';
-                $('#inputEmail')[0].className = 'error-input';
+                continue;
             }
         }  
     }    
     
-}
-
-function chequearMailExistente(chequearYBorrarErrores, $errores, $nodoMail){
-
-    const ALLUSERS = [];
-    const mails = [];
-    const pass = [];
-
-    for(let i=0; i < Object.values(localStorage).length; i++){
-        ALLUSERS.push(JSON.parse(Object.values(localStorage)[i]));
-        mails.push(ALLUSERS[i].user)
-        pass.push(ALLUSERS[i].password)
-
-        if(mails[i] === $nodoMail){
-            chequearYBorrarErrores($errores);
-
-            
-            let errorRegistro = document.createElement("li");
-            errorRegistro.classList.add('error-texto');
-            errorRegistro.innerText = "El mail/usuario ya está en uso. Ingrese otro mail para registrarse."
-            $('#crearEmail')[0].className = 'error-input';
-
-            $errores.appendChild(errorRegistro);
-            $('#crearPassword')[0].className = 'form-control';
-            $('#confirmarPassword')[0].className = 'form-control';
-            
-            return mails[i];
-            break;
-        }
-    }
 }
 
 function chequearYBorrarErrores(input){
