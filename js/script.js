dbd415d2af264eeda1d6976e46c8a414600cab39
Diego <diego.nicolas.gandara@gmail.com>
2020-11-08 19:51:57 -0300

    Agrego funcionalidades manualmente
    Estaba desactualizado el script.js

--- js/script.js
+++ js/script.js
@@ -16,82 +16,93 @@ $nodoBotonLogIn.onclick = function (e){
     const $nodoPass = $('#inputPassword')[0].value;
     const $errores = $('#errores')[0];
     
-    
     const ALLUSERS = JSON.parse(localStorage.getItem('ALLUSERS'));
     const mails = [];
     const pass = [];
-    for(let i=0; i < ALLUSERS.length; i++){
-        mails.push(ALLUSERS[i].user)
-        pass.push(ALLUSERS[i].password)
+
+    if(ALLUSERS === null){
+        const titulo = $('#titulo-login')[0];
+        titulo.innerText = "No hay usuarios registrados. En breve será redireccionado a la pagina de registro para crear su usuario.";
+        titulo.style.color = "red";
+        document.body.style.backgroundColor = "#311c1c";
+        document.querySelector('form').className = "oculto"
+        redireccionar();
+    }else{
+        for(let i=0; i < ALLUSERS.length; i++){
+            mails.push(ALLUSERS[i].user)
+            pass.push(ALLUSERS[i].password)
+        
+            //Si coinciden (LOGIN EXITOSO!):
+            if ($nodoMail === mails[i] && $nodoPass === pass[i]) {
+                let logueado = localStorage.getItem('LOGIN');
     
-        //Si coinciden (LOGIN EXITOSO!):
-        if ($nodoMail === mails[i] && $nodoPass === pass[i]) {
-            let logueado = localStorage.getItem('LOGIN');
-
-            if (logueado === null) {
-                logueado = true;
-                const LOGIN = localStorage.setItem('LOGIN', JSON.stringify(logueado))
-            }else{
-                const LOGIN = logueado;
-                localStorage.setItem('LOGIN', JSON.stringify(LOGIN));
+                if (logueado === null) {
+                    logueado = true;
+                    const LOGIN = localStorage.setItem('LOGIN', JSON.stringify(logueado))
+                }else{
+                    const LOGIN = logueado;
+                    localStorage.setItem('LOGIN', JSON.stringify(LOGIN));
+                }
+    
+                window.location.href = "agregarProductos.html";
+    
             }
-
-            window.location.href = "index.html";
-
-        }
-        else {
-            //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
-            chequearYBorrarErrores($errores);
-
-            let error = document.createElement("li");
-            error.classList.add('error-texto')
-            
-            $errores.appendChild(error);
- 
-            //Si alguno de los campos está vacío:
-            if (!$nodoMail || !$nodoPass) {
-                //Si ambos están vacíos
-                if (!$nodoMail && !$nodoPass) {
-                    error.innerText = "El mail/usuario y la contraseña están incompletos."
+            else {
+                //Chequear si hay errores (li) antes y si hay, eliminarlos, para chequear con 0 li (errores):
+                chequearYBorrarErrores($errores);
+    
+                let error = document.createElement("li");
+                error.classList.add('error-texto')
+                
+                $errores.appendChild(error);
+     
+                //Si alguno de los campos está vacío:
+                if (!$nodoMail || !$nodoPass) {
+                    //Si ambos están vacíos
+                    if (!$nodoMail && !$nodoPass) {
+                        error.innerText = "El mail/usuario y la contraseña están incompletos."
+                        $('#inputPassword')[0].className = 'error-input';
+                        $('#inputEmail')[0].className = 'error-input';
+                        break;
+                    } else if (!$nodoMail && $nodoPass !== "") {
+                        error.innerText = "El mail/usuario está incompleto."
+                        $('#inputMail')[0].className = 'error-input';
+                        break;
+                    }else{
+                        error.innerText = "La contraseña está incompleta."
+                        $('#inputPassword')[0].className = 'error-input';
+                        break;
+                    }
+                }
+                //Si el mail coincide pero el pass no:
+                else if ($nodoMail === mails[i] && $nodoPass !== pass[i]) {
+                    error.innerText = "El password es incorrecto."
+                    $('#inputPassword')[0].className = 'error-input';
+                    $('#inputEmail')[0].className = 'form-control';
+                    // break;
+                }
+                //Si el pass coincide pero el mail no:
+                else if($nodoMail !== mails[i] && $nodoPass === pass[i]){
+                    error.innerText = "El mail/usuario es incorrecto."
+                    $('#inputEmail')[0].className = 'error-input';
+                    $('#inputPassword')[0].className = 'form-control';
+                    // break;
+                } 
+                // Si el mail y el pass no coinciden
+                else if($nodoMail !== mails[i] && $nodoPass !== pass[i]){
+                    error.innerText = "El mail/usuario y la contraseña no existen."
                     $('#inputPassword')[0].className = 'error-input';
                     $('#inputEmail')[0].className = 'error-input';
-                    break;
-                } else if (!$nodoMail && $nodoPass !== "") {
-                    error.innerText = "El mail/usuario está incompleto."
-                    $('#inputMail')[0].className = 'error-input';
-                    break;
-                }else{
-                    error.innerText = "La contraseña está incompleta."
-                    $('#inputPassword')[0].className = 'error-input';
-                    break;
+                    // break;
                 }
-            }
-            //Si el mail coincide pero el pass no:
-            else if ($nodoMail === mails[i] && $nodoPass !== pass[i]) {
-                error.innerText = "El password es incorrecto."
-                $('#inputPassword')[0].className = 'error-input';
-                $('#inputEmail')[0].className = 'form-control';
-                // break;
-            }
-            //Si el pass coincide pero el mail no:
-            else if($nodoMail !== mails[i] && $nodoPass === pass[i]){
-                error.innerText = "El mail/usuario es incorrecto."
-                $('#inputEmail')[0].className = 'error-input';
-                $('#inputPassword')[0].className = 'form-control';
-                // break;
-            } 
-            // Si el mail y el pass no coinciden
-            else if($nodoMail !== mails[i] && $nodoPass !== pass[i]){
-                error.innerText = "El mail/usuario y la contraseña no existen."
-                $('#inputPassword')[0].className = 'error-input';
-                $('#inputEmail')[0].className = 'error-input';
-                // break;
-            }
-            else{
-                continue;
-            }
-        }  
-    }    
+                else{
+                    continue;
+                }
+            }  
+        }    
+
+    }
+
     
 }
 
@@ -102,6 +113,9 @@ function chequearYBorrarErrores(input){
     };
 }
 
+function redireccionar(){
+    setTimeout(function(){window.location.href = "registro.html";}, 4500);
+}
 
 //Funciones de Producto, AgregarProducto
 
