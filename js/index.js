3b86f704be489fc7e4d142356d72952fdc84f579
Diego <diego.nicolas.gandara@gmail.com>
2020-11-08 20:15:54 -0300

    Cambio de nombre al archivo y sus tags script

--- js/index.js
+++
@@ -1,9 +0,0 @@
-console.log("Index.js linkeado a index.html (INVENTARIO)");
-
-const $cerrarSesion = document.querySelector('#cerrarSesion');
-
-$cerrarSesion.onclick = function (e){
-    e.preventDefault();
-    localStorage.removeItem('LOGIN');
-    window.location.href = "login.html";
-}
