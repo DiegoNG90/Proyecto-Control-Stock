84ee013a002984c5908931eefefecf94ab2dfc1b
Diego <diego.nicolas.gandara@gmail.com>
2020-11-09 14:31:42 -0300

    emprolijando y borrando comentarios

--- js/funciones.js
+++ js/funciones.js
@@ -28,21 +28,13 @@ let existente = localStorage.getItem("listadoDeProductos")
         listadoDeProductos.push(producto);
         localStorage.setItem("listadoDeProductos", JSON.stringify(listadoDeProductos));
     }
-
-    // listadoDeProductos.push(producto);
-
-    // localStorage.setItem('producto', JSON.stringify(listadoDeProductos));
-
     campoNombre1.value = "";
     campoCantidad1.value = "";
     campoPrecio1.value = "";
 }
 
-// guardarDatos()
-
 let btn_guardar = document.querySelector(".btn-success")
 btn_guardar.addEventListener("click", guardarDatos)
-
 
 function mostrarDatos(){
     let datosUsuario = JSON.parse(localStorage.getItem('listadoDeProductos'));
@@ -81,47 +73,10 @@ function mostrarDatos(){
                 numero.innerText = i+1;
                 nombre.innerText = datosUsuario[i].nombre;
                 cantidad.innerText = datosUsuario[i].cantidad;
-                precio.innerText = datosUsuario[i].precio;
-            
-
-
-
-            // const nodoDato = document.querySelector(`#producto-${i+1}`)
-            // nodoDato.insertAdjacentElement('beforebegin', `<div> ${datosUsuario[i].nombre} </div>`);
-            // datosProducto.innerHTML += datosUsuario[i].cantidad;
-            // datosProducto.innerHTML += datosUsuario[i].precio;
-            // datosProducto.classList.add("col-12");
-            // datosProducto.style.maxWidth = "70%";
-
-            
-            // //crear boton
-            // let botonEliminar = document.createElement("button");
-            // botonEliminar.classList.add("btn","btn-eliminar1");
-            // botonEliminar.innerHTML = "X";
-            // datosProducto.appendChild(botonEliminar)
-
-            // //crear salto de linea con linea
-            // // let saltoDeLinea = document.createElement("hr");
-            // datosProducto.innerHTML += `<br>`;
-            
-        }
-       
-    // document.querySelector("#numeral1").innerHTML = 1;
-    // document.querySelector("#nombreProducto1").innerHTML = listadoDeProductos[1].nombre;
-    // document.querySelector("#cantidadProducto1").innerHTML = listadoDeProductos[1].cantidad;
-    // document.querySelector("#precioProducto1").innerHTML = listadoDeProductos[1].precio;
-    
-    // document.querySelector("#numeral2").innerHTML = 2;
-    // document.querySelector("#nombreProducto2").innerHTML = listadoDeProductos[2].nombre;
-    // document.querySelector("#cantidadProducto2").innerHTML = listadoDeProductos[2].cantidad;
-    // document.querySelector("#precioProducto2").innerHTML = listadoDeProductos[2].precio;
-
-    // document.querySelector("#numeral3").innerHTML = 3;
-    // document.querySelector("#nombreProducto3").innerHTML = listadoDeProductos[3].nombre;
-    // document.querySelector("#cantidadProducto3").innerHTML = listadoDeProductos[3].cantidad;
-    // document.querySelector("#precioProducto3").innerHTML = listadoDeProductos[3].precio;
-    
+                precio.innerText = datosUsuario[i].precio;            
+        }  
 }
+
 mostrarDatos(); 
 
 
@@ -165,12 +120,6 @@ function borrarItem(){
 }
 
 
-//acá tenemos que hacer un for para aplicar el eventlistener a todos los btn-eliminar
-// let btn_borrarItem = document.querySelector(".btn-eliminar1")
-// btn_borrarItem.addEventListener("click", borrarItem)
-
-// borrarItem()
-
 function borrarTodo(){
     //Previamente podríamos poner un ALERT o un CONFIRM para double-check si el user está seguro de que desea continuar con los cambios.
     const estaSeguro = confirm("¿Está seguro que desea continuar? Se borrará toda la lista y los productos guardados");
@@ -186,8 +135,5 @@ function borrarTodo(){
         $todosLosProductos.forEach(element => document.querySelector("#contenido").removeChild(element));   
     }
 }
-
-// borrarTodo()
-
 let btn_borrarTodo = document.querySelector(".btn-danger");
 btn_borrarTodo.addEventListener("click", borrarTodo);
