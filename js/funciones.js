0659b76ac608e8c577946549774a027d8b2a9177
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2020-11-09 17:11:19 -0300

    Cambio en imputs de formulario de "agregarProductos"
    Se sacaron los required de los imputs, y se agregó un IF en el archivo JS para validar que no se ingresen campos vacios

--- js/funciones.js
+++ js/funciones.js
@@ -10,30 +10,45 @@ function guardarDatos(){
     let campoNombre1 = document.querySelector("#nombre");
     let campoCantidad1 = document.querySelector("#cantidad");
     let campoPrecio1 = document.querySelector("#precio");
+    if (campoNombre1.value==="" || campoCantidad1.value===""|| campoPrecio1.value===""){
+        alert("Favor de completar todos los campos")
+    }else{
+
+        const producto = {
+            nombre: campoNombre1.value,
+            cantidad: campoCantidad1.value,
+            precio: campoPrecio1.value
+        }
     
-    const producto = {
-        nombre: campoNombre1.value,
-        cantidad: campoCantidad1.value,
-        precio: campoPrecio1.value
+    
+    
+    
+    let existente = localStorage.getItem("listadoDeProductos")
+        if (existente === null){
+            existente = [];
+            existente.push(producto);
+            let listadoDeProductos = localStorage.setItem("listadoDeProductos", JSON.stringify(existente))
+        }else{
+            let listadoDeProductos = existente;
+            listadoDeProductos = JSON.parse(listadoDeProductos);
+            listadoDeProductos.push(producto);
+            localStorage.setItem("listadoDeProductos", JSON.stringify(listadoDeProductos));
+        }
+        campoNombre1.value = "";
+        campoCantidad1.value = "";
+        campoPrecio1.value = "";
+    
     }
 
-let existente = localStorage.getItem("listadoDeProductos")
-    if (existente === null){
-        existente = [];
-        existente.push(producto);
-        let listadoDeProductos = localStorage.setItem("listadoDeProductos", JSON.stringify(existente))
-    }else{
-        let listadoDeProductos = existente;
-        listadoDeProductos = JSON.parse(listadoDeProductos);
-        listadoDeProductos.push(producto);
-        localStorage.setItem("listadoDeProductos", JSON.stringify(listadoDeProductos));
-    }
-    campoNombre1.value = "";
-    campoCantidad1.value = "";
-    campoPrecio1.value = "";
+
+
+  
+
+
 }
 
-let btn_guardar = document.querySelector(".btn-success")
+
+let btn_guardar = document.querySelector("#guardarProducto")
 btn_guardar.addEventListener("click", guardarDatos)
 
 function mostrarDatos(){
