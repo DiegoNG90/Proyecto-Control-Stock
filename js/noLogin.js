97529497fa3f9a3c700fa5e9393fd4fa51690648
Diego <diego.nicolas.gandara@gmail.com>
2020-11-11 17:48:11 -0300

    cambio de ruta
    href ahora lleva a la landing page: index.html

--- js/noLogin.js
+++ js/noLogin.js
@@ -1,6 +1,6 @@
 let usuarioLogueado = localStorage.getItem('LOGIN');
 //Si no está logueado, lo mando a login.html
 if (usuarioLogueado === null) {
-    window.location.href = "login.html"
+    window.location.href = "index.html"
 }
 
